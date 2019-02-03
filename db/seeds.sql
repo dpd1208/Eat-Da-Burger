1429a26d59d903e6d288dc6fb5c128728b90b11b
Daniel Duffy <dpd1208@gmail.com>
2019-02-02 20:22:08 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Patty Melt');
+INSERT INTO burgers (burger_name) VALUES ('Jalepeno Cheddar Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fried Chicken Sandwich', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southwest Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
