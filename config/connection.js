503a45af94160d65d5e9d6f41e4e06c584755276
Daniel Duffy <dpd1208@gmail.com>
2019-02-03 22:14:13 -0700

    update connection

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,13 @@
 // Set up MySQL connection
 var mysql = require("mysql");
-var connection;
-
-if (process.env.JAWSBD_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
+var connection = mysql.createConnection({
+    host: "d13xat1hwxt21t45.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "root",
-    database: "burger_db"
+    user: "hw1wvzb3d6hz5jwd",
+    password: "ilr3m6c1n2jh4z4f",
+    database: "ho9nmd5qq5rdb601"
   });
-};
+//};
 
 // Make connection
 connection.connect(function (err) {
