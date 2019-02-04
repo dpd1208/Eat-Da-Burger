b8693fe40d83bb9e177a149c12b7f2c52a5b7c6a
Daniel Duffy <dpd1208@gmail.com>
2019-02-03 20:10:38 -0700

    commit for heroku

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,19 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSBD_URL) {
-   connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burger_db"
-})
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burger_db"
+  });
 };
 
 // Make connection
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
