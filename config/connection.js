425451e4e53c3aa3819f08a2160bd240b3e2b5bd
Daniel Duffy <dpd1208@gmail.com>
2019-02-03 21:52:05 -0700

    update port

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSBD_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 8889,
+    port: 3306,
     user: "root",
     password: "root",
     database: "burger_db"
